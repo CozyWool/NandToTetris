dce5a5fd276a8a357d7c452f6eb48f1fdc7729f2
CozyWool <vsergeev201530@gmail.com>
2026-02-14 09:52:38 +0500

    Parser init

---
+++ 07/VMTranslator/Parser.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+namespace VMTranslator;
+
+public class Parser
+{
+    /// <summary>
+    /// Читает список строк, пропускает строки, не являющиеся инструкциями,
+    /// и возвращает массив инструкций
+    /// </summary>
+    public VmInstruction[] Parse(string[] vmLines)
+    {
+        throw new NotImplementedException();
+    }
+}
