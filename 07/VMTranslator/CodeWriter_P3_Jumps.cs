0fdddb149e67c314da5d9e511e0bab35c4e120bb
CozyWool <vsergeev201530@gmail.com>
2026-03-08 10:25:32 +0500

    Jumps done

--- 07/VMTranslator/CodeWriter_P3_Jumps.cs
+++ 07/VMTranslator/CodeWriter_P3_Jumps.cs
@@ -9,6 +9,39 @@ public partial class CodeWriter
     /// </summary>
     private bool TryWriteProgramFlowCode(VmInstruction instruction, string moduleName)
     {
-        return false;
+        switch (instruction.Name)
+        {
+            case "label":
+                WriteLabel(instruction, moduleName);
+                return true;
+            case "goto":
+                WriteGoto(instruction, moduleName);
+                return true;
+            case "if-goto":
+                WriteIfGoto(instruction, moduleName);
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    private void WriteLabel(VmInstruction instruction, string moduleName)
+    {
+        var label = $"{moduleName}.{instruction.Args[0]}";
+        WriteAsm($"({label})");
+    }
+
+    private void WriteGoto(VmInstruction instruction, string moduleName)
+    {
+        WriteAsm($"@{moduleName}.{instruction.Args[0]}",
+                 "0;JMP");
+    }
+
+    private void WriteIfGoto(VmInstruction instruction, string moduleName)
+    {
+        var label = $"{moduleName}.{instruction.Args[0]}";
+        WritePopToD();
+        WriteAsm($"@{label}",
+                 "D;JNE");
     }
 }
