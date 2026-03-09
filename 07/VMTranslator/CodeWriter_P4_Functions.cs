3c21db7fe396cfb20f6a7fe9956b80ef86affb79
CozyWool <vsergeev201530@gmail.com>
2026-03-09 21:33:18 +0500

    Functions Done

--- 07/VMTranslator/CodeWriter_P4_Functions.cs
+++ 07/VMTranslator/CodeWriter_P4_Functions.cs
@@ -4,6 +4,8 @@ namespace VMTranslator;
 
 public partial class CodeWriter
 {
+    private int _callCount = 0;
+
     /// <summary>
     /// Вставляет вызов функции Sys.init без аргументов
     /// </summary>
@@ -17,6 +19,132 @@ public partial class CodeWriter
     /// </summary>
     private bool TryWriteFunctionCallCode(VmInstruction instruction)
     {
-        return false;
+        switch (instruction.Name)
+        {
+            case "call":
+                WriteCall(instruction);
+                return true;
+            case "function":
+                WriteFunction(instruction);
+                return true;
+            case "return":
+                WriteReturn();
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    private void WriteCall(VmInstruction instruction)
+    {
+        var returnLabel = $"ret.{_callCount++}";
+        var functionName = instruction.Args[0];
+        var nArgs = instruction.Args[1];
+
+        // Создаем новый фрейм вызова
+        SetupNewFrame(nArgs, returnLabel);
+
+        // Прыгаем к функции
+        WriteAsm($"@{functionName}",
+                 "0;JMP");
+
+        // Ставим метку для возврата
+        WriteAsm($"({returnLabel})");
+    }
+
+    private void SetupNewFrame(string nArgs, string returnLabel)
+    {
+        // Записываем адрес возврата
+        WriteAsm($"@{returnLabel}",
+                 "D=A");
+        WritePushD();
+
+        // Сохраняем LCL, ARG, THIS и THAT
+        SaveSegment("LCL");
+        SaveSegment("ARG");
+        SaveSegment("THIS");
+        SaveSegment("THAT");
+
+        // Перемещаем ARG и LCL
+        WriteAsm("@SP",
+                 "D=M",
+                 "@LCL",
+                 "M=D",
+                 "@5", // Высчитываем новый ARG
+                 "D=D-A",
+                 $"@{nArgs}",
+                 "D=D-A",
+                 "@ARG",
+                 "M=D");
+    }
+
+    private void SaveSegment(string segment)
+    {
+        WriteAsm($"@{segment}",
+                 "D=M");
+        WritePushD();
+    }
+
+    private void WriteFunction(VmInstruction instruction)
+    {
+        var functionName = instruction.Args[0];
+        var nArgs = int.Parse(instruction.Args[1]);
+
+        WriteAsm($"({functionName})");
+        for (var i = 0; i < nArgs; i++)
+        {
+            WriteAsm("@0",
+                     "D=A");
+            WritePushD();
+        }
+    }
+
+    private void WriteReturn()
+    {
+        // endFrame = LCL
+        WriteAsm("@LCL",
+                 "D=M",
+                 "@R13", // endFrame
+                 "M=D");
+
+        // returnAddress = *(endFrame - 5)
+        WriteAsm("@5",
+                 "A=D-A",
+                 "D=M",
+                 "@R14", // returnAddress
+                 "M=D");
+
+        // *ARG = pop()
+        WritePopToD();
+        WriteAsm("@ARG",
+                 "A=M",
+                 "M=D");
+
+        // SP = ARG + 1
+        WriteAsm("@ARG",
+                 "D=M+1",
+                 "@SP",
+                 "M=D");
+
+        RestoreSegment("THAT", 1);
+        RestoreSegment("THIS", 2);
+        RestoreSegment("ARG", 3);
+        RestoreSegment("LCL", 4);
+
+        // goto returnAddress
+        WriteAsm("@R14",
+                 "A=M",
+                 "0;JMP");
+    }
+
+    private void RestoreSegment(string segment, int offset)
+    {
+        WriteAsm("@R13",
+                 "D=M",
+                 $"@{offset}",
+                 "A=D-A",
+                 "D=M",
+                 $"@{segment}",
+                 "M=D");
     }
 }
