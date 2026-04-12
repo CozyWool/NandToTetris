ea83870e74255557d97d4c113814a4e6f587733c
CozyWool <vsergeev201530@gmail.com>
2026-04-12 12:47:22 +0500

    CodeWriter ArraysAndStrings done

--- 10/JackCompiler/CodeWriter_P4_ArraysAndStrings.cs
+++ 10/JackCompiler/CodeWriter_P4_ArraysAndStrings.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Linq;
+
 namespace JackCompiling
 {
     public partial class CodeWriter
@@ -7,7 +10,23 @@ namespace JackCompiling
         /// </summary>
         private bool TryWriteStringValue(TermSyntax term)
         {
-            return false;
+            if (term is not ValueTermSyntax valueTerm || valueTerm.Value.TokenType != TokenType.StringConstant)
+            {
+                return false;
+            }
+
+            var stringConstant = valueTerm.Value.Value;
+
+            Write($"push constant {stringConstant.Length}");
+            Write("call String.new 1");
+
+            foreach (var asciiCode in stringConstant.Select(symbol => (int) symbol))
+            {
+                Write($"push constant {asciiCode}");
+                Write("call String.appendChar 2");
+            }
+
+            return true;
         }
 
         /// <summary>
@@ -15,7 +34,22 @@ namespace JackCompiling
         /// </summary>
         private bool TryWriteArrayAccess(TermSyntax term)
         {
-            return false;
+            if (term is not ValueTermSyntax valueTerm || valueTerm.Indexing is null)
+            {
+                return false;
+            }
+
+            var varInfo = FindVarInfo(valueTerm.Value.Value)
+                          ?? throw new Exception($"Unknown variable {valueTerm.Value.Value}");
+
+            Write($"push {varInfo.SegmentName} {varInfo.Index}");
+            WriteExpression(valueTerm.Indexing.Index);
+            Write("add");
+
+            Write("pop pointer 1");
+            Write("push that 0");
+
+            return true;
         }
 
         /// <summary>
@@ -23,7 +57,26 @@ namespace JackCompiling
         /// </summary>
         private bool TryWriteArrayAssignmentStatement(StatementSyntax statement)
         {
-            return false;
+            if (statement is not LetStatementSyntax let || let.Index is null)
+            {
+                return false;
+            }
+
+            var varInfo = FindVarInfo(let.VarName.Value)
+                          ?? throw new Exception($"Unknown variable {let.VarName.Value}");
+
+            Write($"push {varInfo.SegmentName} {varInfo.Index}");
+            WriteExpression(let.Index.Index);
+            Write("add");
+
+            WriteExpression(let.Value);
+
+            Write("pop temp 0");    // сохранить значение правой части
+            Write("pop pointer 1"); // THAT = левая часть (адрес)
+            Write("push temp 0");   // вернуть значение правой части
+            Write("pop that 0");    // левая часть = правая часть
+
+            return true;
         }
     }
 }
